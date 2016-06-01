2859e4ba2d48a4640c14dcf80b727c630c29addf
Raul Quispe <raul.quispe@live.com>
2016-06-01 17:12:18 -0500

    Initial Commit

---
+++ Bimonts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bimonts
+//
+//  Created by Desarrollador iOS on 6/1/16.
+//  Copyright © 2016 QuimaDevelopers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Bimonts/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Bimonts
+//
+//  Created by Desarrollador iOS on 6/1/16.
+//  Copyright © 2016 QuimaDevelopers. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
